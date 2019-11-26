155b61f480e5aa5c2212ee0b57f16ecbce174762
aobeirne20 <56138845+aobeirne20@users.noreply.github.com>
2019-11-26 19:23:11 +0000

    new AppFrames

--- AppFramesMain.py
+++ AppFramesMain.py
@@ -1,5 +1,6 @@
-
+import numpy as np
 import tkinter as tk
+import Main
 
 
 class App:
@@ -9,6 +10,8 @@ class App:
         self.init_ui(master)
         self.init_bind()
         self.outline = None
+        self.ink_matrix = np.zeros((28, 28))
+        self.draw_canvas = None
 
     def init_ui(self, master):
 
@@ -18,22 +21,59 @@ class App:
         self.outline.master.resizable(0,0)
         self.outline.pack()
 
-        self.draw_frame = tk.Frame(self.outline, bg='#E4E4E4', width=400, height=400, cursor='dot')
-        buttons_frame = tk.Frame(self.outline, bg='cyan', width=350, height=40)
-        output_frame = tk.Frame(self.outline, bg='black', width=350, height=150)
+        self.draw_frame = tk.Canvas(self.outline, bg='#E4E4E4', width=392, height=392, cursor='dot')
+        buttons_frame = tk.Frame(self.outline, bg='#3B3E3F', width=350, height=40)
+        self.output_frame = tk.Frame(self.outline, bg='#1F1F1F', width=350, height=150)
         data_frame = tk.Frame(self.outline, bg='#2F3937', width=350, height=120)
 
-        self.draw_frame.grid(column=0, rowspan=3, padx=50, pady=50)
-        output_frame.grid(row=0, column=1, padx=(0, 50), pady=(50, 0))
+        self.draw_frame.grid(column=0, rowspan=3, padx=58, pady=58)
+        self.output_frame.grid(row=0, column=1, padx=(0, 50), pady=(50, 0))
         data_frame.grid(row=1, column=1, padx=(0, 50), pady=(10, 80))
-        buttons_frame.grid(row=2, column=1, padx=(0, 50), pady=(0, 50))
+        buttons_frame.grid(row=2, column=1, padx=(0, 50), pady=(0, 50), sticky=tk.W)
+
+        clear_button = tk.Button(buttons_frame, text='CLEAR', command=self.clear_canvas, bg='#3C3C3C', fg='#BDBDBD')
+        clear_button.pack(side=tk.LEFT, padx=(0, 50))
+
+        run_button = tk.Button(buttons_frame, text='RUN', command=self.run_net, bg='#3C3C3C', fg='#BDBDBD')
+        run_button.pack(side=tk.LEFT)
+
+        self.num_input = tk.Canvas(self.output_frame, bg='#E4E4E4', width=112, height=112)
+        self.num_output = tk.Frame(self.output_frame, bg='#E4E4E4', width=112, height=112)
+        self.num_input.grid(row=0, column=0, padx=(19, 44), pady=19)
+        self.num_output.grid(row=0, column=1, padx=(44, 19), pady=19)
+
+    def init_bind(self):
+        self.draw_frame.bind("<B1-Motion>", self.mouse_click)
+        self.draw_frame.bind("<Button-1>", self.mouse_click)
 
     def mouse_click(self, event):
         self.draw_frame.focus_set()
-        print("clicked at", event.x, event.y)
+        pixel_x = event.x // 14
+        pixel_y = event.y // 14
+        if self.ink_matrix[pixel_y, pixel_x] == 0:
+            self.render_canvas(pixel_x, pixel_y)
+            self.render_mini_canvas(pixel_x, pixel_y)
+            self.ink_matrix[pixel_y, pixel_x] = 1
 
-    def init_bind(self):
-        self.draw_frame.bind("<Button-1>", self.mouse_click)
+    def render_canvas(self, pixel_x, pixel_y):
+        self.draw_frame.create_rectangle(pixel_x*14, pixel_y*14, (pixel_x+1)*14, (pixel_y+1)*14, fill='black')
+
+    def render_mini_canvas(self, pixel_x, pixel_y):
+        self.num_input.create_rectangle(pixel_x*4, pixel_y*4, (pixel_x+1)*4, (pixel_y+1)*4, fill='black')
+
+    def clear_canvas(self):
+        self.draw_frame.delete('all')
+        self.num_input.delete('all')
+        self.ink_matrix = np.zeros((28, 28))
+
+    def run_net(self):
+        probs = Main.use_net(self.ink_matrix)
+        print(probs)
+        value = np.argmax(probs)
+
+        value_disp = tk.Text(self.num_output, height=1, width=1)
+        value_disp.pack()
+        value_disp.insert(tk.END, value)
 
 
 root = tk.Tk()
--- Main.py
+++ Main.py
@@ -2,9 +2,7 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 from torch.autograd import Variable
-import matplotlib.pyplot as plt
-import numpy as np
-from time import time
+import numpy
 
 device = torch.device('cuda')
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
@@ -31,21 +29,27 @@ class Net(torch.nn.Module):
         x = self.fc3(x)
         return torch.nn.functional.log_softmax(x)
 
+    def read(self, x):
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return torch.nn.functional.softmax(x)
+
 
 net = Net()
-net = net.cuda()
+net = net
 
 optimizer = torch.optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
 criterion = torch.nn.NLLLoss()
 
-n_epochs = 5
+n_epochs = 3
 log_interval = 1
 
-start_time = time()
+
 for epoch in range(n_epochs):
     for batch_idx, (data, target) in enumerate(train_loader):
-        data = data.cuda()
-        target = target.cuda()
+        data = data
+        target = target
         data, target = Variable(data), Variable(target)
         data = data.view(-1, 28*28)
         optimizer.zero_grad()
@@ -58,11 +62,10 @@ for epoch in range(n_epochs):
                                                                            len(train_loader.dataset),
                                                                            100. * batch_idx / len(train_loader),
                                                                            loss.data.item()))
-end_time = time()
-print(end_time-start_time)
-
-
-
-
-
+def use_net(image_matrix):
+    tensor_input = transform(image_matrix)
+    tensor_input = tensor_input.float()
+    tensor_input = tensor_input.view(-1, 28*28)
+    probs = net.read(tensor_input)
+    return probs.detach().numpy()
 
